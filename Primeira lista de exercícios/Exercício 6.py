820e8eccc39630cf61dfe0b58e493be9a8c4bb43
Anajamily19 <jamilebarboza.0007@gmail.com>
2026-08-30 19:19:08 -0300

    Sexto exercício

---
+++ Primeira lista de exercícios/Exercício 6.py
@@ -0,0 +1,8 @@
+name = str(input("What's your name? "))
+age = int(input("How old are you? "))
+
+if age >=18:
+    print("Great! You are of legal age.")
+
+else:
+    print("Sorry! Your are not of legal age.")
