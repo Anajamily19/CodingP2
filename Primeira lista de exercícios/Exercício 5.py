38602368f0fdf6d6e00d467e144997e32bf8a108
Anajamily19 <jamilebarboza.0007@gmail.com>
2026-08-30 19:06:37 -0300

    Quinto exercício

---
+++ Primeira lista de exercícios/Exercício 5.py
@@ -0,0 +1,11 @@
+
+first_note = int(input("Enter the first note: "))
+second_note = int(input("Enter the second note: "))
+
+average = (first_note + second_note)/2
+
+print(f"Your average is: {average}")
+
+#Primeiro peço a primeira e a segunda nota
+#logo após calculo a média
+#apresento o resultado ao usuário
