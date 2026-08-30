85d0007eded25bbbdc3215a967d89b84922a28f6
Anajamily19 <jamilebarboza.0007@gmail.com>
2026-08-30 18:54:35 -0300

    Quarto exercício

---
+++ Primeira lista de exercícios/Exercício 4.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+
+double = num * 2
+
+print(f"The double that number is: {double}")
+
+#Primeiro pedi ao usuário o valor
+#Após isso calculei o dobro
+#Em seguida mostrei o resultado ao usuário
