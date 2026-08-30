b6c9dfd1b3dfc4157fcb71ca034f261f14e908c6
Anajamily19 <jamilebarboza.0007@gmail.com>
2026-08-30 18:16:48 -0300

    Segundo exercício

---
+++ Primeira lista de exercícios/Exercício 2.py
@@ -0,0 +1,11 @@
+#Soma de dois números
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
+sum = num_1 + num_2
+
+print(f"A soma é: {sum}")
+
+#Primeiro eu pedir ao usúario por meio do input o primeiro e o segundo valor
+#Após fiz a soma das duas variáveis 
+#E apresentei ao usuário o resultado dessa soma
