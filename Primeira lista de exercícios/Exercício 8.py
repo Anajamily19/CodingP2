5c9c1d722bebe6ccab53f55381d73352c972a2aa
Anajamily19 <jamilebarboza.0007@gmail.com>
2026-08-30 19:37:46 -0300

    Oitavo exercício

---
+++ Primeira lista de exercícios/Exercício 8.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+
+if num >=1:
+    print("The number is positive")
+
+elif num ==0:
+    print("Your number is neutral")
+
+else:
+    print("Your number is negative")
