c7da2ba155407463b9dd5a5fb76d7fea1d2cc8ca
Anajamily19 <jamilebarboza.0007@gmail.com>
2026-08-30 18:40:36 -0300

    Terceiro exercício

---
+++ Primeira lista de exercícios/Exercício 3.py
@@ -0,0 +1,14 @@
+num = int(input("Enter the first number "))
+
+successor = num + 1
+
+print(f"The sucessor is: {successor}")
+
+predecessor = num - 1
+
+print(f"The predecessor is: {predecessor}")
+
+#Primeiramente pedi ao usuário o valor desejado
+#logo após calculei o sucessor com a variável + 1 que significa o próximo número
+#E o antecessor com a variável -1 indicando o valor menor do que a entrada
+#Mostrei ao usuário o resultado por meio do "print"
