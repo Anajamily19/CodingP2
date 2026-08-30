246887ad313f7d1f9ab0997ccfa817c1157bc893
Anajamily19 <jamilebarboza.0007@gmail.com>
2026-08-30 19:44:28 -0300

    Nono exercício

---
+++ Primeira lista de exercícios/Exercício 9.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 > num2:
+    print("The first number is larger")
+elif num2 > num1:
+    print("The second number is large")
+else:
+    print("The two numbers are equal.")
